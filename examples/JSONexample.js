bcfc7abdeabc49edb04035505d362e3d14762c8d
Alan O <alan@buyhouse.co.uk>
2012-10-24 13:45:42 +0100

    example data provided

---
+++ examples/JSONexample.js
@@ -0,0 +1,86 @@
+﻿var List = [{
+    props: {
+        "data-id": "r1-c0-sC0",
+        text: "one",
+        title: "description one" },
+    sub: [{
+        props: {
+            "data-id": "r1-c1-sC0",
+            text: "one:A",
+            title: "description one:A" } }, {
+        props: {
+            "data-id": "r1-c2-sC0",
+            text: "one:B",
+            title: "description one:B" } }, {
+        props: {
+            "data-id": "r1-c3-sC0",
+            text: "one:C",
+            title: "description one:C" } } ] }, {
+    props: {
+        "data-id": "r2-c0-sC0",
+        text: "two",
+        title: "description two" },
+    sub: [{
+        props: {
+            "data-id": "r2-c1-sC0",
+            text: "two:A",
+            title: "description two:A" } }, {
+        props: {
+            "data-id": "r2-c2-sC0",
+            text: "two:B",
+            title: "description two:B" } }, {
+        props: {
+            "data-id": "r2-c3-sC0",
+            text: "two:C",
+            title: "description two:C" } } ] }, {
+    props: {
+        "data-id": "r3-c0-sC0",
+        text: "three",
+        title: "description three" },
+    sub: [{
+        props: {
+            "data-id": "r3-c1-sC0",
+            text: "three:A",
+            title: "description three:A" } }, {
+        props: {
+            "data-id": "r3-c2-sC0",
+            text: "three:B",
+            title: "description three:B" } }, {
+        props: {
+            "data-id": "r3-c3-sC0",
+            text: "three:C",
+            title: "description three:C" } } ] }, {
+    props: {
+        "data-id": "r4-c0-sC0",
+        text: "four",
+        title: "description four" },
+    sub: [{
+        props: {
+            "data-id": "r4-c1-sC0",
+            text: "four:A",
+            title: "description four:A" } }, {
+        props: {
+            "data-id": "r4-c2-sC0",
+            text: "four:B",
+            title: "description four:B" } }, {
+        props: {
+            "data-id": "r4-c3-sC0",
+            text: "four:C",
+            title: "description four:C" } } ] }, {
+    props: {
+        "data-id": "r5-c0-sC0",
+        text: "five",
+        title: "description five" },
+    sub: [{
+        props: {
+            "data-id": "r5-c1-sC0",
+            text: "five:A",
+            title: "description five:A" } }, {
+        props: {
+            "data-id": "r5-c2-sC0",
+            text: "five:B",
+            title: "description five:B" } }, {
+        props: {
+            "data-id": "r5-c3-sC0",
+            text: "five:C",
+            title: "description five:C" } } ] } ];
